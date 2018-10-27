0c30a4500512cd174e70cd7d4a49ce7b542d3db1
Andrea Bruno <andrea_bruno@hotmail.com>
2018-10-28 06:58:03 +0700

    Adapted for the new network library

--- Node/Startup.cs
+++ Node/Startup.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+
+
 namespace Node
 {
   public class Startup
   {
+    NetworkManager.Device Device;
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
@@ -23,18 +27,20 @@ namespace Node
         app.UseDeveloperExceptionPage();
       }
       System.Net.ServicePointManager.DefaultConnectionLimit = 10;
-      String MyAddress = null;
+      string MyAddress = null;
+      Dictionary<String, String> EntryPoints = null;
       if (env.IsDevelopment())
       {
+        EntryPoints = new Dictionary<String, String>() { { "http://localhost:62430", Environment.MachineName }, { "http://localhost:55008", Environment.MachineName } };
 #if DEBUG
         MyAddress = "http://localhost:55007";
 #elif DEBUGNETWORK
         MyAddress = "http://localhost:55008";
 #endif
       }
-      var Network = BlockchainManager.HookToNetwork.Initialize(MyAddress);
+      Device = BlockchainManager.NetworkInitializer.HookToNetwork(EntryPoints, MyAddress);
       //NetworkManager.Network.Initialize();
-      Network.Test();
+      //Network.Test();
       app.Run(async (context) =>
       {
         if (context.Request.Method == "POST")
@@ -46,7 +52,7 @@ namespace Node
           foreach (var item in context.Request.Form)
             Form.Add(item.Key, item.Value);
           using (System.IO.Stream Stream = new System.IO.MemoryStream())
-            if (NetworkManager.Network.OnReceivesHttpRequest(QueryString, Form, context.Connection.RemoteIpAddress.ToString(), out string ContentType, Stream))
+            if (Device.OnReceivesHttpRequest(QueryString, Form, context.Connection.RemoteIpAddress.ToString(), out string ContentType, Stream))
             {
               context.Response.ContentType = ContentType;
               Stream.Position = 0;
@@ -57,7 +63,6 @@ namespace Node
         }
         await context.Response.WriteAsync("Online!");
       });
-
     }
   }
 }
